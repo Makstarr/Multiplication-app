c36a0e46c6a3bdbd0ea6d669c7bbc0f3ea68203b
Makstarr <maksi112/@>
2020-02-17 18:49:37 +0300

    Initial Commit

---
+++ Multiplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multiplication
+//
+//  Created by Максим on 17.02.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
